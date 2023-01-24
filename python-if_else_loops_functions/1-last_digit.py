98f971abd989114c63656f13275199aba6958b14
Natha0b <laura.barona04@gmail.com>
2023-01-24 15:34:24 -0500

    add information in the file last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 
-lastdigit = number % 10
-
+if number < 0:
+    lastdigit = (number *-1) % 10
+    lastdigit = -1 * lastdigit
+else:
+    lastdigit = number % 10
 if lastdigit > 5:
     print("Last digit of", number, "is", lastdigit, "and is greater than 5")
 elif lastdigit == 0:
     print("Last digit of", number, "is", lastdigit, "and is 0")
 elif lastdigit < 6:
-    print("Last digit of", number, "is", lastdigit, "and is less than 6 and not 0")
+    print("Last digit of", number, "is", lastdigit, 
+    "and is less than 6 and not 0")
