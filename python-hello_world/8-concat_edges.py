10468b8402f749c34a7ecf0d277edff482e4da14
Natha0b <laura.barona04@gmail.com>
2023-01-23 21:26:23 -0500

    add information in the file 8 concat

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-
-print(f"{str[38:66]}" + f"{str[106:112]}" + f"{str[0:6]}")
+str = str[38:66] + str[106:112] + str[0:6]
+print(str)
