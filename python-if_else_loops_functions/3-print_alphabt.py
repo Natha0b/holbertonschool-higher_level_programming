b1e4315523f2878375c845e3d3d1377187c744c9
Natha0b <laura.barona04@gmail.com>
2023-01-24 16:24:35 -0500

    add changes in the file 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for letter_code in range(ord('a'), ord('z')+1):
     letter = chr(letter_code)
     if letter not in "qe":
-        print("{}".format (letter), end="")
+        print("{}".format(letter), end="")
