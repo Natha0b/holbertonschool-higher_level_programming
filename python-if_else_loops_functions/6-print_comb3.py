a3e44028d8b6afaa1c1fccf5d8621980c5578401
Natha0b <laura.barona04@gmail.com>
2023-01-25 20:43:20 -0500

    add information in the file 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for number_1 in range(0, 10):
     for number_2 in range(number_1 + 1, 10):
         if number_1 == 8:
-            print(f"{number_1}{number_2}")
+            print("{}{}".format(number_1, number_2))
         else:
-            print(f"{number_1}{number_2}", end=", ")
+            print("{}{}".format(number_1, number_2), end=", ")
