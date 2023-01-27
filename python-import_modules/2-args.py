15e9fcb97acbd803932ff5663c3d1be766d6288b
Natha0b <laura.barona04@gmail.com>
2023-01-27 15:19:48 -0500

    add information in the file 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+
+    args = len(argv) - 1
+    if args == 0:
+        print("0 arguments.")
+    else:
+        if args == 1:
+            print("{:d} argument:".format(args))
+        else:
+            print("{:d} arguments:".format(args))
+        for i in range(args):
+            print(f"{i + 1}: {args[i]}")
