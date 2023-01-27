6e0f367036ecffdfc4c83a270bdc92adcb079348
Natha0b <laura.barona04@gmail.com>
2023-01-27 09:54:40 -0500

    add information in the file 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from sys import argv
-argv = argv[1:]
+arg = argv[1:]
 if __name__ == '__main__':
     r = 0
-    for a in argv:
+    for a in arg:
         r += int(a)
     print(r)
