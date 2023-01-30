9b3dc3ec3243dbfaa5dc36f12f967d489926cdab
Natha0b <laura.barona04@gmail.com>
2023-01-30 17:11:23 -0500

    add information in the file 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+
+    new = my_list[:]
+
+    if idx < 0:
+        return new
+    if idx > (len(new)):
+        return new
+    new[idx] = element
+    return new
