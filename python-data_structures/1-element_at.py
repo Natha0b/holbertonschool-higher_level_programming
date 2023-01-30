6d86ef2ee3be51ddfd60f2fc7318004cc4b8c9ec
Natha0b <laura.barona04@gmail.com>
2023-01-30 16:22:24 -0500

    add information in the file 3

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,8 +3,7 @@ def element_at(my_list, idx):
 
     if idx < 0:
         return None
-    if idx == my_list:
-        print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+    if idx > (len(my_list)):
+        print("Element at index {:d} is {}".format(idx, my_list))
     else:
         return None
-    
