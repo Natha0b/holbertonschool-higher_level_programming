3dec6fcc30544b0c996df01fb9d91d630a43261c
Natha0b <laura.barona04@gmail.com>
2023-01-31 17:13:00 -0500

    add changes in the file 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a_tuple = tuple_a + (0, 0)
+    b_tuple = tuple_b + (0, 0)
+    return (a_tuple[0] + b_tuple[0], a_tuple[1] + b_tuple[1])
