8a03f574b1d412f8a3a05a64da8b4b6155975a7f
Natha0b <laura.barona04@gmail.com>
2023-01-31 18:13:03 -0500

    changes in the file 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = my_list[0]
-    for number in my_list:
-        if number > max:
-            max = number
+    if my_list:
+        max = my_list[0]
+        for number in my_list:
+            if number > max:
+                max = number
         return max
     return None
