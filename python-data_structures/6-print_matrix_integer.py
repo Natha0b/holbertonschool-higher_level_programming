6c9e23d05ada7d29ff3317f6de33e9bb61d8591d
Natha0b <laura.barona04@gmail.com>
2023-01-31 16:32:05 -0500

    add changes in the file 2

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for line in matrix:
-        print("{}".format((line)))
+    for line in range(len(matrix)):
+        for line2 in range(len(matrix[line])):
+            print("{:d}".format(matrix[line][line2]), end="")
+            if line2 < len(matrix[line]) - 1:
+                print(" ", end="")
+        print('')
