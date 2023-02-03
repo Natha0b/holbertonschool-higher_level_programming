fc4cc6d209496aba538641880368608e35c4810d
Natha0b <laura.barona04@gmail.com>
2023-02-02 19:06:47 -0500

    add changes in the file 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,6 +4,8 @@ def roman_to_int(roman_string):
     roman_numbers = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
         }
+    if type(roman_string) != str or roman_string is None:
+        return num
     i = 0
     num = 0
     while i < len(roman_string):
