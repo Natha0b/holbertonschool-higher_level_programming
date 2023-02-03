eeeb1f2b7cf42521d47eb80b6fda43815ff3d93d
Natha0b <laura.barona04@gmail.com>
2023-02-03 11:15:20 -0500

    add information in the file 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    p = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            p += 1
+        except IndexError:
+            break
+    print()
+    return p
