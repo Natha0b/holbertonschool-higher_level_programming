62eb2883ce132a28d445091d2202b8ec4926e296
Natha0b <laura.barona04@gmail.com>
2023-02-02 19:09:17 -0500

    add changes in the file 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,10 +4,12 @@ def roman_to_int(roman_string):
     roman_numbers = {
         'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900
         }
-    if type(roman_string) != str or roman_string is None:
-        return num
+
     i = 0
     num = 0
+    if type(roman_string) != str or roman_string is None:
+        return num
+
     while i < len(roman_string):
         if i + 1 < len(roman_string)and roman_string[i:i + 2]in roman_numbers:
             num += roman_numbers[roman_string[i:i + 2]]
