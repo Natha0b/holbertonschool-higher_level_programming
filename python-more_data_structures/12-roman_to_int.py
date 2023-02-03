6d792c2e7792e3eae7be9240ce6fba25655bacf9
Natha0b <laura.barona04@gmail.com>
2023-02-02 19:05:39 -0500

    add changes in the file 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
 
     roman_numbers = {
-        'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
         }
     i = 0
     num = 0
