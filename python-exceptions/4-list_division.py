451192e363c610f19dd9ddf89a99b76263df5ce4
Natha0b <laura.barona04@gmail.com>
2023-02-03 19:46:44 -0500

    add changes in the file 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -11,8 +11,4 @@ def list_division(my_list_1, my_list_2, list_length):
             value = 0
             print("division by 0")
         except IndexError:
-            value = 0
-            print("out of range")
-        finally:
-            new.append(value)
-    return new
+     
