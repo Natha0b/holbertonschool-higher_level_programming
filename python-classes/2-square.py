9bbee426b4ee71d3afcd120e5862581dfe92237d
Natha0b <laura.barona04@gmail.com>
2023-02-06 20:13:28 -0500

    add information in the file 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""create class square"""
+
+
+class Square:
+    """class square"""
+    def __init__(self, size=0):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
