b8c53d18e6c1ab0f235a024f2314686770f170a9
Natha0b <laura.barona04@gmail.com>
2023-02-07 16:39:28 -0500

    add information in the file 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""create class square"""
+
+
+class Square:
+    """class square"""
+    def __init__(self, size=0):
+        """Initialization of Square object with size.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if self.__size != 0:
+            for i in range(self.__size):
+                print("#" * self.__size)
+        else:
+            print("")
