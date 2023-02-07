95baf48ec9acb0cc26e4ae25aee7b43c75ae3d23
Natha0b <laura.barona04@gmail.com>
2023-02-06 21:00:17 -0500

    add changes in the file 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,15 +7,16 @@ class Square:
     """class square"""
     def __init__(self, size=0):
         """Initialization of Square object with size.
-        """  
+        """
         self.size = size
+
     @property
     def size(self):
         return self.__size
 
     @size.setter
     def size(self, value):
-        
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
 
