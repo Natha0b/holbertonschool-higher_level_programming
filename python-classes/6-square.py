a99ffbe157ffc0924a3c1eb4b812a564636886f9
Natha0b <laura.barona04@gmail.com>
2023-02-07 17:44:00 -0500

    add changes in the file 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -23,14 +23,21 @@ class Square:
 
         if value < 0:
             raise ValueError("size must be >= 0")
+
+        self.__size = value
     
     @property
     def position(self):
         return self.__position
 
-    
-
-        self.__size = value
+    @position.setter
+    def position(self, value):
+        if not (isinstance(value, tuple) and
+                len(value) == 2 and
+                all(isinstance(v, int) for v in value) and
+                all(v >= 0 for v in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
 
