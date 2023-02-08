c9fdea5ae5d195e383a0130d925851c135a7153b
Natha0b <laura.barona04@gmail.com>
2023-02-08 18:08:07 -0500

    add information in the file 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""function that adds 2 integers."""
+
+
 def add_integer(a, b=98):
+
+    """function that adds 2 integers, a TypeError exception with the message"""
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
