b76be0541387ea3afa7200f451687a7f0bcb13bd
Natha0b <laura.barona04@gmail.com>
2023-02-10 12:31:29 -0500

    add information in the file 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" function that prints a text with 2 new lines"""
+
+
+def text_indentation(text):
+    """prints a text with 2 new lines"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    chars = [".", ":", "?"]
+
+    for aux, i in enumerate(text):
+        if i in chars:
+            print(i, end="\n")
+            print()
+
+        elif i == " ":
+            if text[aux-1] in chars:
+                continue
+            else:
+                print(i, end="")
+        else:
+            print(i, end="")
