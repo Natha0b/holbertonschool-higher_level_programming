daebf79e24848b814f5b5607a91dd406b8e4fc8c
Natha0b <laura.barona04@gmail.com>
2023-02-09 20:53:02 -0500

    add information in the file 3

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""function that divides all elements of a matrix."""
+
+
+def matrix_divided(matrix, div):
+
+    """that divides all elements of a matrix"""
+
+    
+
+
+
+
---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""function that divides all elements of a matrix."""
+
+
+def say_my_name(first_name, last_name=""):
+    """function that divides all elements of a matrix."""
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+        
+    print("My name is {} {}".format(first_name, last_name))
