df0d6c37aa8eefd105edb31cdbad4cd7383fe6ab
Natha0b <laura.barona04@gmail.com>
2023-02-16 11:33:18 -0500

    add changes in the file 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,12 +8,15 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == '__main__':
+def main():
     args = sys.argv[1:]
     filename = "add_item.json"
     try:
         file = load_from_json_file(filename)
-    except FileNotFoundError:
+    except Exception:
         file = []
     file.extend(args)
     save_to_json_file(file, filename)
+
+if __name__ == '__main__':
+    main()
