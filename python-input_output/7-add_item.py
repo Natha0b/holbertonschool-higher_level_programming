a6ca8902a4d24e8bf84e463178d4f0ebd47f5636
Natha0b <laura.barona04@gmail.com>
2023-02-16 11:31:34 -0500

    add changes in the file 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,16 +3,17 @@
 ''' script that adds all arguments to a Python list'''
 
 import sys
+import json
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
 
 if __name__ == '__main__':
     args = sys.argv[1:]
     filename = "add_item.json"
     try:
-        new = load_from_json_file(filename)
-    except Exception:
-        new = []
-    new.extend(args)
-    save_to_json_file(new, filename)
+        file = load_from_json_file(filename)
+    except FileNotFoundError:
+        file = []
+    file.extend(args)
+    save_to_json_file(file, filename)
