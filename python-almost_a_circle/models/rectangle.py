dbba660cf5e7da199274159e27b5bb7c526cc940
Natha0b <laura.barona04@gmail.com>
2023-02-21 15:26:20 -0500

    add changes in the file

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -3,7 +3,7 @@
 from models.base import Base
 
 
-class Rectangle:
+class Rectangle(Base):
     """the class Rectangle that inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
 
