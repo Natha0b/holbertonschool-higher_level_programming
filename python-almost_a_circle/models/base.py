ba955b9eada26dec907d9ba23abcb00bb59ec1fd
Natha0b <laura.barona04@gmail.com>
2023-02-23 18:17:10 -0500

    add changes in the file base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -15,7 +15,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """that returns the JSON string representation
