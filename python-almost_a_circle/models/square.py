643b644f9b1ee6d641e0fe157e200f931c080cda
Natha0b <laura.barona04@gmail.com>
2023-02-22 20:18:43 -0500

    add changes in the file 10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,3 +8,9 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
 
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """String representation of a square."""
+        
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
+            self.id, self.x, self.y, self.width)
