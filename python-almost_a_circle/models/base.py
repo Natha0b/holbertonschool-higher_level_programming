78e7eb2260e9e8a62d6f9a2d28717c979e2dcc25
Natha0b <laura.barona04@gmail.com>
2023-02-23 19:22:18 -0500

    add changes in the file base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -38,6 +38,7 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        list = []
         if json_string is None or len(json_string) == 0:
-            return []
-        return(json.loads(json_string))
+            return list
+        return json.loads(json_string)
--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """create class Rectangle"""
-from rectangle import Rectangle
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
