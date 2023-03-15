73ae979f6dfe494f5c5e14dc23c4e3fdea405989
Natha0b <laura.barona04@gmail.com>
2023-03-15 17:47:14 -0500

    add information in the file 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 -- Creates the database and the table.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE states (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id), UNIQUE (id), name VARCHAR(256));
+CREATE TABLE states (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256), PRIMARY KEY (id), UNIQUE (id));
