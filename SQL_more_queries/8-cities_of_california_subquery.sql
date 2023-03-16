ff5777a8d361c6cc85d49aa3590f12e1af0f8e3d
Natha0b <laura.barona04@gmail.com>
2023-03-15 20:36:48 -0500

    add information in the file 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Lists all the cities of California that can be found in the database.
-SELECT 'id', 'name' FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id;
+SELECT 'id', 'cities' FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id;
