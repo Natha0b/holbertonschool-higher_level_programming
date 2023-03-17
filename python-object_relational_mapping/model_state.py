90e55341443c393bd1a3a9f0357acc7730257e52
Natha0b <laura.barona04@gmail.com>
2023-03-17 15:48:37 -0500

    add changes in the file model_state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,6 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
+    """Class state"""
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True)
