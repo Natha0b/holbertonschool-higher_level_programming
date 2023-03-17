59d05c5703367694a6e6c5f91bf2dcc8dd35bda4
Natha0b <laura.barona04@gmail.com>
2023-03-17 14:29:03 -0500

    add information in the file 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    matchName = sys.argv[4]
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=database, charset="utf8")
@@ -20,7 +21,7 @@ if __name__ == "__main__":
                 JOIN states \
                     ON cities.state_id = states.id\
                 WHERE states.name=%s\
-                ORDER by cities.id ASC")
+                ORDER by cities.id ASC", (matchName,))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
