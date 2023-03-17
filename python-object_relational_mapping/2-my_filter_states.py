d167a871fcec678f5ca7a68ec5ef377d9f6ac68e
Natha0b <laura.barona04@gmail.com>
2023-03-16 21:09:35 -0500

    add information in the file 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that lists all states from the database
+"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    matchName = sys.argv[4]
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database, charset="utf8")
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM states WHERE name LIKE '{matchName}'")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
