3e840ba15260ef6241624a6753a28c3b69c618a9
Natha0b <laura.barona04@gmail.com>
2023-03-17 09:53:49 -0700

    add information in the file 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE" (f"%{matchName}%",))
+    cur.execute("SELECT * FROM states "
+            "WHERE BINARY states.name='{}' ".format(matchName))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
