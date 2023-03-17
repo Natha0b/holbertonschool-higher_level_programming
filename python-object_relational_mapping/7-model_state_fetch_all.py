2f407b75b1a9f2afecff3b13898ecc96804164f2
Natha0b <laura.barona04@gmail.com>
2023-03-17 16:19:11 -0500

    add changes in the file 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,15 +3,14 @@
 lists all State objects from the database hbtn_0e_6_usa
 """
 
+
 import sys
 from model_state import State
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    # Arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -23,9 +22,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()
-
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+    for state in session.query(State).order_by(State.id).all():
+        print("{:d}: {}".format(state.id, state.name))
 
     session.close()
