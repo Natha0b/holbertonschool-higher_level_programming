96e76751da511d4eb2fd83dec46f85edc45f00ff
Natha0b <laura.barona04@gmail.com>
2023-03-16 19:15:57 -0500

    add changes in the file 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -14,8 +14,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY'N%'"
-                "ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY'N%'")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
