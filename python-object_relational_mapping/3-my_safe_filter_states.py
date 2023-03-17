b7dc27a114613b2d56547a2bf6d7e9ee234c57c7
Natha0b <laura.barona04@gmail.com>
2023-03-17 13:44:48 -0500

    add changes in the repo

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,15 +7,17 @@ Third MySQLdb script
 import MySQLdb
 import sys
 
-if __name__ ==  "__main__":
+if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     matchName = sys.argv[4]
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM  states WHERE states.name=%s ORDER BY states.id""", (matchName,))
+    cur.execute("""SELECT * FROM  states WHERE states.name=%s
+                ORDER BY states.id""", (matchName,))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
