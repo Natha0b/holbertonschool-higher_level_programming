33fb5067979c4c91ace60358821e7d1e1d67776c
Natha0b <laura.barona04@gmail.com>
2023-03-17 16:27:39 -0500

    add changes in the file 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,6 +15,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """class state"""
     tablename = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
