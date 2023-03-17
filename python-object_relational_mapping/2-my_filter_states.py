eded70fcf9dba6cabe7dcbf44d0df1d34aecea47
Natha0b <laura.barona04@gmail.com>
2023-03-16 21:38:23 -0500

    add information in the file 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
-    cur.execute(f"SELECT * FROM states WHERE name LIKE '{matchName}'")
+    cur.execute("SELECT * FROM states WHERE name LIKE" (f"%{matchName}%",))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
