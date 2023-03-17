2b5dfa166cb7bf338811716c6766a45949aaa378
Natha0b <laura.barona04@gmail.com>
2023-03-17 16:11:21 -0500

    add changes in the file 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,9 +4,10 @@ lists all State objects from the database hbtn_0e_6_usa
 """
 
 import sys
+from model_state import State
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 
 if __name__ == "__main__":
